932aef3a5a01f33938ec67e80fee3e6deef59e9a
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-27 13:07:18 +0600

    app.js-working

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.get('/test', (req, res) => {
     res.send("About Route View");
 });
 
+
+app.get('/contact', (req, res) => {
+    res.send("About Route View");
+});
+
+app.get('/term',(req,res)=>{
+    res.send(
+        {
+            id:1,
+            name:"jamil",
+        }
+    )
+})
+
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
 });
