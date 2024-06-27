f13a56be4d60318444b308d0bf679d880a4df426
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-27 17:37:29 +0600

    app-js

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
-const express = require('express');
+
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose").set("debug", true);
+
+const productRouter = require('./routes/productRoute');
+
+
+// console.log("base url",process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI, {
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}' from ${req.ip}`);
+  res.header("Access-Control-Allow-Origin", "*"); // Be more specific in production
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.send("jamil Hossain");
 });
 
-app.get('/about', (req, res) => {
-    res.send("About Route View");
-});
+app.use('/api', productRouter);
 
-app.get('/about', (req, res) => {
-    res.send("About Route View");
-});
-
-app.get('/test', (req, res) => {
-    res.send("About Route View");
-});
-
-
-app.get('/contact', (req, res) => {
-    res.send("About Route View");
-});
-
-app.get('/term',(req,res)=>{
-    res.send(
-        {
-            id:1,
-            name:"jamil",
-        }
-    )
-})
-
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
