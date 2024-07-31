01b12df687f06f13b640d789277187d21baf8bcc
jamil.hossain <jamil.hossain@nexdecade.com>
2024-07-31 16:18:34 +0600

    Product.js-update-schema

--- models/Product.js
+++ models/Product.js
@@ -13,7 +13,7 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-});
+},{ timestamps: true });
 
 const Product = mongoose.model('Product', productSchema);
 
